b000a1a818973a804863f95deeaa6e005885f79d
Carson <azzlamusic@hotmail.com>
2024-06-01 23:40:39 -0600

    reorganized assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,41 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const proxima_nova = localFont({
+  src: [
+    {
+      path: 'fonts/Proxima-Nova-Regular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: 'fonts/Proxima-Nova-Semibold.otf',
+      weight: '500',
+      style: 'bold',
+    },
+    {
+      path: 'fonts/Proxima-Nova-Bold.otf',
+      weight: '600',
+      style: 'bold',
+    },
+    {
+      path: 'fonts/Proxima-Nova-Extrabold.otf',
+      weight: '700',
+      style: 'bold',
+    },
+  ],
+})
+
+const interstate_condensed = localFont({
+  src: [
+    {
+      path: 'fonts/interstate-25.ttf',
+      weight: '400',
+      style: 'normal'
+    }
+  ]
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +49,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={proxima_nova.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,111 +2,120 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col justify-between">
+      {/* Hero Image*/}
+      <div className="flex flex-col justify-between items-center bg-header-image bg-cover h-[500px] pointer-events-none ">
+        <div className="flex items-center justify-between text-lg nav pt-16">
+          <a>APPOINTMENTS</a>
+          <a>MEDICAL CARD INFO</a>
+          <a>FAQs</a>
+          <a>PHARMACIES</a>
+          <a>ABOUT</a>
+        </div>
+        {/* Logo */}
+        <img className="mx-auto max-w-[40%]" src="/UtahGrownLogoWhiteBorder.png"></img>
+      </div>
+      
+      {/* Upcoming Events */}
+      <div className="flex items-center justify-between flex-col space-y-4">
+        <h3>Utah's Cannabis Community</h3>
+        <h1>Upcoming Utah Medical Card Events</h1>
+        <div className="flex flex-col items-center justify-between space-y-4">
+          <div className="flex justify-between items-center bg-grey-three border border-grey rounded-lg">
+            <p className="px-8">Downtown SLC</p>
+            <div className="flex flex-col px-8">
+              <h2>Tuesday, October 3rd 2023</h2>
+              <p>9:00 am - 2:00pm</p>
+              <p>New Patients and Renewals</p>
+            </div>
+            <div className="btn-primary px-8">Book Time</div>
+          </div>
+          <div className="flex justify-between items-center bg-grey-three border border-grey rounded-lg">
+            <p className="px-8">Price UT</p>
+            <div className="flex flex-col px-8">
+              <h2>Thursday, October 19th 2023</h2>
+              <p>9:00 am - 4:00pm</p>
+              <p>Renewals Only</p>
+            </div>
+            <div className="btn-primary px-8">Book Time</div>
+          </div>
+          <div className="flex justify-between items-center bg-grey-three border border-grey rounded-lg">
+            <p className="px-8">&lt;Other Location&gt;</p>
+            <div className="flex flex-col px-8">
+              <h2>Friday, November 10th 2023</h2>
+              <p>10:00 am - 2:00pm</p>
+              <p>Special Event for Veterans</p>
+            </div>
+            <div className="btn-primary px-8">Book Time</div>
+          </div>
+        </div>
+      </div>
+      
+      {/* Steps */}
+      <div className="flex flex-col items-center justify-between w-[800px] mx-auto">
+        <h1>Things to know before your appointment</h1>
+        <div className="flex w-full space-x-8">
+          {/* Item 1 */}
+          <img className="mb-auto" src="/Number1.png"></img>
+          <div className="flex flex-col">
+            <p>You’re going to register with the state of Utah. You can start this before you arrive.</p>
+            <div className="flex">
+              <div className="btn-primary px-16">UtahID.org</div>
+              <p>Obtain access to your Utah Digital ID</p>
+            </div>
+            <div className="flex">
+              <div className="btn-primary px-16">EVS.Utah.gov</div>
+              <p>Fill out all the fields until you get to “Awaiting Certification” status</p>
+            </div>
+            <img className="max-w-[200px] mx-auto" src="/AwaitingCert-Image.png"></img>
+          </div>
+        </div>
+
+        {/* Item 2 */}
+        <div className="flex w-full space-x-8">
+          <img className="mb-auto" src="/Number2.png"></img>
+          <p>Check in with your QMP</p>
+          <ol className="list-decimal">
+            <li>QMP Registration</li>
+            <li>Medical Evaluation/Consultation</li>
+            <li>Payment</li>
+          </ol>
+        </div>
+
+        {/* Item 3 */}
+        <div className="flex w-full space-x-8">
+          <img className="mb-auto" src="/Number3.png"></img>
+          <p>Renewal or New Patient?</p>
+          <div className="flex flex-col">
+            <p>If this is a renewal, congratulations you are ready to shop...</p>
+            <p>If you are a new patient, here are some helps to get you started...</p>
+            <ol className="list-decimal self-center">
+              <li>QMP Registration</li>
+              <li>Medical Evaluation/Consultation</li>
+              <li>Payment</li>
+              <span>**Dont forget to take advantage of new patient deals**</span>
+            </ol>
+          </div>
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
+      <img className="max-w-[800px] self-center" src="/MockBannerAd.png"></img>
+      <div className="flex max-w-[800px] self-center">
+        <img className="max-w-half" src="/Consultation.png"></img>
+        <div className="flex flex-col">
+          <h1>Utah Medical Cards</h1>
+          <p>
+            A medical cannabis card, also known as a medical marijuana
+            card, is a state-issued identification card that allows
+            qualified patients to legally purchase and use medical
+            cannabis for the treatment of specific medical conditions.
+            In Utah, a card is required to access and purchase medical
+            cannabis products from state-licensed dispensaries.
+            Having a medical card does not allow you to possess any
+            cannabis product but only the products you purchased
+            under your PAT (Medical Card ID number).
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </div>
       </div>
     </main>
   );
