486d3b50caae8c1e8f3628ea9ac079a1ff6ed4dc
Carson <azzlamusic@hotmail.com>
2024-06-01 23:41:04 -0600

    extended exports

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,26 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+        'green-one': '#103c00',
+        'green-two': '#35711f',
+        'green-three': '#60a543',
+        'green-four': '#e6efc5',
+        'tan-one': '#4f463c',
+        'tan-two': '#e7cbaf',
+        'grey-one': '#333333',
+        'grey-two': '#808080',
+        'grey-three': '#f2f2f2'
+    },
+    maxWidth: {
+      'half': '50%',
+    },
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "header-image": "url('./assets/TopHeaderImage.png')",
+        "header-image-2x": "url('./assets/TopHeaderImage@2x.png')",
+        "banner-ad": "url('./assets/MockBannerAd.png')",
+        "banner-ad-2x": "url('./assets/MockBannerAd@2x.png')",
       },
     },
   },
